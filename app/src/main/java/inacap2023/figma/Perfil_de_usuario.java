c9feaf050a399005dcb1a078d4bd5d22028e5387
fidyrovak <ivan.h6@hotmail.com>
2023-11-22 09:49:27 -0300

    Crear activity_perfil_de_usuario en java

---
+++ app/src/main/java/inacap2023/figma/Perfil_de_usuario.java
@@ -0,0 +1,14 @@
+package inacap2023.figma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Perfil_de_usuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_de_usuario);
+    }
+}
