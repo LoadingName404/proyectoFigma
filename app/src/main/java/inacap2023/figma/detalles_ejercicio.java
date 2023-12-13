ed103c3c05e0a8d6abc6416dadfa3a922b50cd2f
fidyrovak <ivan.h6@hotmail.com>
2023-12-13 01:12:20 -0300

    ingreso de interfaz del detalle del ejercicio

---
+++ app/src/main/java/inacap2023/figma/detalles_ejercicio.java
@@ -0,0 +1,14 @@
+package inacap2023.figma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class detalles_ejercicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalles_ejercicio);
+    }
+}
