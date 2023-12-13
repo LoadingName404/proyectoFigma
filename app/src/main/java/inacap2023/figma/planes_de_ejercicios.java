25e583da1d6d972b56445c5f9f0125292a90f393
Loading Name... <wiliamsvillaroel@gmail.com>
2023-12-12 23:44:14 -0300

    probando cosas

--- app/src/main/java/inacap2023/figma/planes_de_ejercicios.java
+++ app/src/main/java/inacap2023/figma/planes_de_ejercicios.java
@@ -5,10 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class planes_de_ejercicios extends AppCompatActivity {
 
+    ArrayAdapter<plan> adapter;
+    ArrayList<plan> listado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,5 +29,24 @@ public class planes_de_ejercicios extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        this.listado = new ArrayList<plan>();
+        this.adapter = new ArrayAdapter<plan>(this, android.R.layout.simple_list_item_1);
+
+        plan c1 = new plan("Plan 1", "Este es el plan 1", "https://i.imgur.com/1QZzJ9o.png");
+        plan c2 = new plan("Plan 2", "Este es el plan 2", "https://i.imgur.com/1QZzJ9o.png");
+        plan c3 = new plan("Plan 3", "Este es el plan 3", "https://i.imgur.com/1QZzJ9o.png");
+
+        this.listado.add(c1);
+        this.listado.add(c2);
+        this.listado.add(c3);
+
+        this.adapter.add(c1);
+        this.adapter.add(c2);
+        this.adapter.add(c3);
+
+        ListView listViewPlanes = findViewById(R.id.listViewPlanes);
+
+        listViewPlanes.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/inacap2023/figma/read_plan.java
@@ -0,0 +1,14 @@
+package inacap2023.figma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class read_plan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read_plan);
+    }
+}
